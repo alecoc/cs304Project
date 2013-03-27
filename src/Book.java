c7b08f40ab3171e9f0746d43cfe4f6465582661b
Alec O'Connor <the_hungriest_hippo@hotmail.com>
2013-03-27 10:52:28 -0700

    Modified book variables, still missing a couple.  Added methods to Clerk class

--- src/Book.java
+++ src/Book.java
@@ -4,8 +4,11 @@ public class Book<name, copies> {
 	 * Creation of a Book tuple, containing the name and the number of copies of a book
 	 */
 	
-  public final String name; 
-  public final int copies; 
+  public String name; 
+  public int copies; 
+  public int callNumber;
+  public int status; //0 is on-hold, 1 is in, 2 is out.  Could go about it that way.
+  
   public Book(String name, int copies) { 
     this.name = name; 
     this.copies = copies; 
