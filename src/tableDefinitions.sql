47cd27d1c0d813e549e2b8492abd0bb4ae30a8ed
Alec O'Connor <the_hungriest_hippo@hotmail.com>
2013-03-28 10:03:02 -0700

    Fixed the sql table, this executed correctly in sqlplus, so our tables are made

--- src/tableDefinitions.sql
+++ src/tableDefinitions.sql
@@ -9,14 +9,12 @@ CREATE TABLE Borrower(
       expiryDate int,
       type varchar(20),
       PRIMARY KEY(bid)
-)
-GO
+);
 CREATE TABLE BorrowerType (
-      type varchar(20 NOT NULL,
+      type varchar(20) NOT NULL,
       bookTimeLimit int,
       PRIMARY KEY(type)
-)
-GO
+);
 CREATE TABLE Book (
       callNumber int NOT NULL,
       isbn int,
@@ -25,35 +23,30 @@ CREATE TABLE Book (
       publisher varchar(20),
       year int,
       PRIMARY KEY(callNumber)
-)
-GO
+);
 CREATE TABLE HasAuthor (
      callNumber int NOT NULL,
      name varchar(30) NOT NULL,
      PRIMARY KEY(callNumber, name)
-)
-GO
+);
 CREATE TABLE HasSubject (
      callNumber int NOT NULL,
      subject varchar(20) NOT NULL,
      PRIMARY KEY(callNumber, subject)
-)
-GO
+);
 CREATE TABLE BookCopy (
      callNumber int NOT NULL,
      copyNo int NOT NULL,
-     status varchar,
+     status int,
      PRIMARY KEY (callNumber, copyNo)
-)
-GO
+);
 CREATE TABLE HoldRequest (
      hid int NOT NULL,
      bid int,
      callNumber int,
      issuedDate varchar(20),
      PRIMARY KEY (hid)
-)
-GO
+);
 CREATE TABLE Borrowing (
      borid int NOT NULL,
      bid int,
@@ -62,13 +55,12 @@ CREATE TABLE Borrowing (
      outDate varchar(20),
      inDate varchar(20),
      PRIMARY KEY(borid)
-)
-GO
+);
 CREATE TABLE Fine (
      fid int NOT NULL,
      amount float,
-     issuedDate varchar(20)
-     paidDate varchar(20)
+     issuedDate varchar(20),
+     paidDate varchar(20),
      borid int,
      PRIMARY KEY(fid)
 );
